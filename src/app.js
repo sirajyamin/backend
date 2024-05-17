a32f9a2326a0afd1c6bb95dd189618f67ca795b4
sirajyamin <sirajyamin.dev@gmail.com>
2024-05-17 19:02:15 +0500

    improved

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+	})
+);
+app.use(express.json({ limit: "16kb" }));
+
+export default app;
