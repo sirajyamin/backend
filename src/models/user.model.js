a9b3b0e1478c7c5f36b94972a7247154254a438e
sirajyamin <sirajyamin.dev@gmail.com>
2024-05-19 18:27:41 +0500

    user and video model updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,5 +57,33 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
 	return await bcrypt.compare(password, this.password);
 };
+userSchema.methods.generateAccessTokens = async function () {
+	jwt.sign(
+		{
+			_id: this._id,
+			email: this.email,
+			username: this.username,
+			fullname: this.fullname,
+		},
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: process.env.ACCESS_TOKEN_EXPIRT,
+		}
+	);
+};
+userSchema.methods.generateRefreshTokens = async function () {
+	jwt.sign(
+		{
+			_id: this._id,
+			email: this.email,
+			username: this.username,
+			fullname: this.fullname,
+		},
+		process.env.REFRESH_TOKEN_SECRET,
+		{
+			expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+		}
+	);
+};
 
 export const User = mongoose.model("User", userSchema);
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,5 +1,5 @@
 class ApiError extends Error {
-	constructor(statusCode, message = "Whatever", error = [], statck = "") {
+	constructor(statusCode, message = "Api Error", error = [], statck = "") {
 		super(message);
 		this.statusCode = statusCode;
 		this.data = null;
